af6b54e8dbc8e1fcf4fe5b30dac21fdc216d387c
GA9BR1 <gustavoalberttodev@gmail.com>
2022-10-11 14:59:38 -0300

    adicionada nova rota SSR

--- pages/[about].tsx
+++ pages/[about].tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Container } from '../styles/about'
 
 
+
 export default function About() {
     return (
         <Container>
@@ -9,5 +10,6 @@ export default function About() {
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&aposs standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             <Image src='/images/charizard.png' width='300' height='300' alt='charizard'/>
         </Container>
+
     )
 }
